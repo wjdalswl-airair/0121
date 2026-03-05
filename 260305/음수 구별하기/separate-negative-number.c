1b334df41097e694516ad32f415030f334388096
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-05 09:29:13 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260305/음수 구별하기/separate-negative-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    printf("%d\n",n);
+    if (n<0) {
+        printf("minus");
+    }
+    return 0;
+}
