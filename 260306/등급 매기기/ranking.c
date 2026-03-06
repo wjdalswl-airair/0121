981f2afefce2677981037d310ddcd3879dc68615
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-06 10:15:25 +0900

    Title: 등급 매기기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260306/등급 매기기/ranking.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int scroe;
+    scanf("%d", &score)
+
+    if (score >= 90) {
+        printf("A");
+    }
+    else if (score >= 80) {
+        printf("B");
+    }
+    else if (score >= 70) {
+        printf("C");
+    }
+    else if (score >= 60) {
+        printf("D");
+    }
+    else {
+        printf("F");
+    }
+    return 0;
+}
