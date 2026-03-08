93ae5d6ecb245ba11bb898408fa58d77988585c6
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-08 13:53:38 +0900

    Title: 정수 입력받아 배수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260308/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,i;
+    scanf("%d",&n);
+
+    for (i=1; i<6; i++) {
+        printf("%d ",i*n);
+
+    }
+    return 0;
+}
