820cf4f1c0857ff7b448c793dda4a5d176b1e0b3
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-08 13:38:01 +0900

    Title: 문자 출력하기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260308/문자 출력하기/print-text.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    int i;
+    
+    scanf("%c",&a);
+
+    for (i=0; i<8; i++) {
+        printf("%c",a);
+    }
+    return 0;
+}
