e53d8fe0181a69cc889c1969a1eaee1c5e6590ec
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-08 13:40:30 +0900

    Title: 문자열 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260308/문자열 출력하기/print-string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,i;
+    scanf("%d",&n);
+
+    for (i=0; i<n; i++) {
+        printf("LeebrosCode\n");
+    }
+    
+
+    return 0;
+}
