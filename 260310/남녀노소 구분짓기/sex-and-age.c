9641b7023b8d8c42ee975ef00d08df06e534ba25
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-10 09:39:09 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260310/남녀노소 구분짓기/sex-and-age.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int sex, age;
+    scanf("%d\n%d",&sex, &age);
+    if (sex == 0) {
+        if (age >= 19) {
+            printf("MAN");
+        }
+        else {
+            printf("BOY");
+        }
+    else {
+       if (age >= 19) {
+            printf("WOMAN");
+        }
+        else {
+            printf("GIRL");
+        } 
+    }
+    }
+    return 0;
+}
