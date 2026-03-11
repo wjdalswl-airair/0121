065658327606e885674fd5ca50935e56396a3240
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-11 13:56:13 +0900

    Title: 배열의 합 계산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260311/배열의 합 계산하기/calculate-sum-of-array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr[5] = {3,1,4,5,9};
+    int result;
+    result = arr[1]+arr[3]+arr[4];
+    printf("%d", result);
+    return 0;
+}
