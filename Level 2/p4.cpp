67de6fff4392e872a5cb0e23c139c702beebf9ca
JEONG MINJI <jmj20021030@gmail.com>
2026-03-11 16:48:19 +0900

    3번씩 출력

---
+++ Level 2/p4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	scanf("%d %d %d", &a, &b, &c);
+	printf("%d%d%d\n", a, a, a);
+	printf("%d%d%d\n", b, b, b);
+	printf("%d%d%d\n", c, c, c);
+
+	return 0;
+}
