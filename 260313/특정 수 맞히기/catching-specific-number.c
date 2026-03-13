83ee816e5beab09a53e12af5c53429157d6b3dcc
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-13 11:43:18 +0900

    Title: 특정 수 맞히기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260313/특정 수 맞히기/catching-specific-number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int flag = 0;
+    while (flag == 0) {
+        int n;
+        scanf("%d",&n);
+        if (n<25) {
+            printf("Higher");
+        }
+        else if (n>25) {
+            printf("Lower");
+        }
+        else {
+            printf("Good");
+            flag = 1;
+        }
+    }
+    return 0;
+}
