430e0737631f30124887d8dff5cc0321889acf47
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-13 10:56:12 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260313/0 나올때까지 출력/print-until-zero-is-given.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int i = 1;
+    while (1) {
+        int i;
+        scanf("%d",&i);
+        if(i==0 ){
+            break;
+        }
+        else {
+            printf("%d\n",i);
+        }
+   
+
+        
+    }
+
+    return 0;
+}
