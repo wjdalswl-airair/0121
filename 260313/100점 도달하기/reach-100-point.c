ea706d71919bc1adeec90e6b08ae29197f28896d
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-13 09:08:10 +0900

    Title: 100점 도달하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260313/100점 도달하기/reach-100-point.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    scanf("%d",&a);
+
+    for (int i = a; i<=100;i++) {
+        if (i >= 90) {
+            printf("A ");
+        }
+        else if (i >= 80) {
+            printf("B ");
+        }
+        else if (i >= 70) {
+            printf("C ");
+        }
+        else if (i >= 60) {
+            printf("D ");
+        }
+        else {
+            printf("F ");
+        }
+    }
+    
+    return 0;
+}
