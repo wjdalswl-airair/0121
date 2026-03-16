cb6fe507f3cded56e134e76fa474cb4d8e1aec7e
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-16 11:45:22 +0900

    Title: 좌우로 반복하며 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260316/좌우로 반복하며 출력/print-in-left-right-reverse.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n;
+    scanf("%d",&n);
+
+    for (int i=0;i<n;i++) {
+        if (i%2==0) {
+            for (int j=1; j<=n;j++) {
+            printf("%d",j);
+        } 
+        }
+        else if (i%2!=0) {
+            for (int j=n; j>=1;j--) {
+            printf("%d",j);
+        } 
+        }
+        printf("\n");
+    }
+    return 0;
+}
