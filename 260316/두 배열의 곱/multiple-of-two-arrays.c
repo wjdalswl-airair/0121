6c5e10f70f4026397fb6e16d307cb9ec8843c62a
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-16 11:20:37 +0900

    Title: 두 배열의 곱, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 260316/두 배열의 곱/multiple-of-two-arrays.c
+++ 260316/두 배열의 곱/multiple-of-two-arrays.c
@@ -8,8 +8,21 @@ int main() {
         for (int j=0;j<3;j++) {
             int num;
             scanf("%d ",&num);
-            arr[i][j] = num;
+            arr1[i][j] = num;
         }
+    }
+    for (int i=0; i<3;i++) {
+        for (int j=0;j<3;j++) {
+            int num;
+            scanf("%d ",&num);
+            arr2[i][j] = num;
+        }
+    }
+    for (int i=0; i<3;i++) {
+        for (int j=0;j<3;j++) {
+            printf("%d ",arr1[i][j]*arr2[i][j]);
+        }
+    printf("\n");
     }
     return 0;
 }
