b22bc2ed96b48b234dce801a71e00f2858c2fe88
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-16 11:19:20 +0900

    Title: 두 배열의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 260316/두 배열의 곱/multiple-of-two-arrays.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr1[3][3];
+    int arr2[3][3];
+    for (int i=0; i<3;i++) {
+        for (int j=0;j<3;j++) {
+            int num;
+            scanf("%d ",&num);
+            arr[i][j] = num;
+        }
+    }
+    return 0;
+}
