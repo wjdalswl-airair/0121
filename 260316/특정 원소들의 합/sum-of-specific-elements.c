b343e7a1493912d70371cbd52167a01189be5270
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-16 11:36:28 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260316/특정 원소들의 합/sum-of-specific-elements.c
+++ 260316/특정 원소들의 합/sum-of-specific-elements.c
@@ -4,7 +4,7 @@ int main() {
     // Please write your code here.
     int total=0;
     for (int i=0;i<4;i++) {
-        for (int j=0; j<=4;j++) {
+        for (int j=0; j<4;j++) {
             int num;
             scanf("%d",&num);
             if (j<=i) {
