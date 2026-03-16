6f0be4d69ec86a5b084719bf25934bd2423d2071
wjdalswl-airair <162536914+wjdalswl-airair@users.noreply.github.com>
2026-03-16 11:35:58 +0900

    Title: 특정 원소들의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260316/특정 원소들의 합/sum-of-specific-elements.c
+++ 260316/특정 원소들의 합/sum-of-specific-elements.c
@@ -4,10 +4,13 @@ int main() {
     // Please write your code here.
     int total=0;
     for (int i=0;i<4;i++) {
-        for (int j=0; j<=i;j++) {
+        for (int j=0; j<=4;j++) {
             int num;
             scanf("%d",&num);
-            total += num;
+            if (j<=i) {
+                total += num;
+            }
+            
         }
     }
     printf("%d",total);
